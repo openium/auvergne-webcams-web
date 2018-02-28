1a84517a7e28f59296a720bd4e7723fdfc184ec4
Alexandre Caillot <a.caillot@openium.fr>
2018-02-28 17:58:08 +0100

    Refonte graphique + Affichage des webcams

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import logo from '../../resources/images/logo.svg';
 import playStoreBadge from '../../resources/images/play-store-badge.svg'
 import appStoreBadge from '../../resources/images/app-store-badge.svg'
 import './App.css';
 import sectionsDatas from '../../resources/json/aw-config';
-import Section from '../Section/Section'
+import Section from '../../components/Section/Section'
 
 class App extends Component {
     render() {
@@ -14,27 +15,31 @@ class App extends Component {
         });
 
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Auvergne Webcams</h1>
-                </header>
-                <div>
-                    {sections}
+            <MuiThemeProvider>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">Auvergne Webcams</h1>
+                    </header>
+
+                    <div>
+                        {sections}
+                    </div>
+
+                    <div>
+                        <p className="App-intro">
+                            Auvergne webcams est disponible au téléchargement sur <b>iOS</b> et <b>Android</b> !
+                        </p>
+                        <a style={{marginRight: '10px'}} href='https://itunes.apple.com/app/id1183930829'>
+                            <img height="75px" alt="Disponible sur Google Play" src={appStoreBadge}/>
+                        </a>
+                        <a style={{marginLeft: '10px'}}
+                           href='https://play.google.com/store/apps/details?id=fr.openium.auvergnewebcams&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                            <img height="75px" alt="Disponible sur l'App Store" src={playStoreBadge}/>
+                        </a>
+                    </div>
                 </div>
-                <div>
-                    <p className="App-intro">
-                        Auvergne webcams est disponible au téléchargement sur <b>iOS</b> et <b>Android</b> !
-                    </p>
-                    <a style={{marginRight: '10px'}} href='https://itunes.apple.com/app/id1183930829'>
-                        <img height="75px" alt="Disponible sur Google Play" src={appStoreBadge}/>
-                    </a>
-                    <a style={{marginLeft: '10px'}}
-                       href='https://play.google.com/store/apps/details?id=fr.openium.auvergnewebcams&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
-                        <img height="75px" alt="Disponible sur l'App Store" src={playStoreBadge}/>
-                    </a>
-                </div>
-            </div>
+            </MuiThemeProvider>
         );
     }
 }
