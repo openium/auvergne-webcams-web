6c20d404450d34446e9433a8b218dfae55249a1a
Thomas LEDUC <thomaslmoi15@hotmail.fr>
2023-02-07 15:21:43 +0100

    fix add missing import in Section entity

--- src/Entity/Section.php
+++ src/Entity/Section.php
@@ -9,6 +9,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
 #[ORM\Entity(repositoryClass: SectionRepository::class)]
 class Section
